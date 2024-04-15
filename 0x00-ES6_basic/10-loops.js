5a7fb00ff2dc0c2bba119f732ef15a8a3026885e
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-15 09:41:20 +0300

    task 12

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
+  for (const value of array) {
     const idx = array.indexOf(value);
     array[idx] = appendString + value; // eslint-disable-line no-param-reassign
   }

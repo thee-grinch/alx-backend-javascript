4741f3394d4d222b2adab8f0aa7e16a55687ada3
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-16 22:40:13 +0300

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return uploadPhoto().then((photo) => createUser().then((user) => {
-    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  }).catch(() => console.log('Signup system offline')));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }

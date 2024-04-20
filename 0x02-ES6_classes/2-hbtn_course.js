1de3d9f427907bec53e2922be460941eb8475ff9
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-20 20:55:24 +0300

    task 3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -19,6 +19,9 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
     this._name = newName;
   }
 
@@ -27,6 +30,9 @@ export default class HolbertonCourse {
   }
 
   set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length must be a number');
+    }
     this._length = newLength;
   }
 
@@ -35,6 +41,9 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw TypeError('Students must be an array');
+    }
     this._students = newStudents;
   }
 }

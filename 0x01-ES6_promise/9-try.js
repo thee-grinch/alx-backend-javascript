237f1cf73b348d75a13f55c7a64b313046cf2972
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-20 16:02:40 +0300

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,6 +1,6 @@
 export default function guardrail(mathFunction) {
   try {
-    return [`${mathFunction()}`, 'Guardrail was processed'];
+    return [mathFunction(), 'Guardrail was processed'];
   } catch (e) {
     return [`${e}`, 'Guardrail was processed'];
   }

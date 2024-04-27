9a9e42932755223132e0c705aaa90c8420e25753
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-27 09:14:22 +0300

    task 9

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,12 +1,44 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        if (typeof size !== 'number') {
-            throw TypeError('size should be a Number');
-        }
-        if (typeof location !== 'string') {
-            throw TypeError('location should be a string');
-        }
-        this._size = size;
-        this._location = location;
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw TypeError('size should be a Number');
     }
+    if (typeof location !== 'string') {
+      throw TypeError('location should be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('size should be a Number');
+    }
+    this._size = value;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw TypeError('location should be a string');
+    }
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this;
+  }
 }

b40d8200955c34519daa0c997234c345778ddda4
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-27 22:35:56 +0300

    task 1 and 2

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ export default function get_list_students() { //eslint-disable-line
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return students;
 }

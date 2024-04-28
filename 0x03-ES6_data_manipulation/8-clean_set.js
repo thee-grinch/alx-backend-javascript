8a5e54d22dd5ebf67d8317c6e6e5f6c1e43d0c47
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-28 13:13:51 +0300

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString) {
+    return '';
+  }
+  let result = '';
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result += `${value.slice(startString.length)}-`;
+    }
+  }
+
+  // Remove the trailing dash
+  result = result.slice(0, -1);
+
+  return result;
+}
